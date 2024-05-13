90f27414159132afd69afee804f1fd1fdebd4991
Kushaga Prasad <kushagraprasad24@gmail.com>
2024-05-13 17:48:31 +0530

    Gateway service code

---
+++ src/main/java/com/gateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableEurekaClient
+public class SpringcloudGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringcloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GatewayService/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
