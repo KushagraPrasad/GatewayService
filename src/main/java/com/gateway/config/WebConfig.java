ab8fa6bb1a3b3ae0d52ebba58c9c649102183043
Kushaga Prasad <kushagraprasad24@gmail.com>
2024-05-24 16:38:00 +0530

    Cors config fixed

--- src/main/java/com/gateway/config/WebConfig.java
+++ src/main/java/com/gateway/config/WebConfig.java
@@ -12,12 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowCredentials(true);
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD").allowedHeaders("*").allowCredentials(true);
 	}
 
 	@Bean
 	public WebClient.Builder webClientBuilder() {
 		return WebClient.builder();
 	}
-
 }
--- src/main/java/com/gateway/controller/GatewayController.java
+++ src/main/java/com/gateway/controller/GatewayController.java
@@ -2,10 +2,11 @@ package com.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.gateway.dto.LoginResponse;
@@ -15,7 +16,8 @@ import com.gateway.dto.RegisterUserDto;
 import reactor.core.publisher.Mono;
 
 @CrossOrigin
-@Controller
+@RequestMapping("/auth")
+@RestController
 public class GatewayController {
 
 	private final WebClient.Builder webClientBuilder;
