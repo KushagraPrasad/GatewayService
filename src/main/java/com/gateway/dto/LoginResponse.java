f055888a6d9f82bdbfd7c71e05738cdc10729b0f
Kushaga Prasad <kushagraprasad24@gmail.com>
2024-05-26 12:56:03 +0530

    login response update

--- src/main/java/com/gateway/dto/LoginResponse.java
+++ src/main/java/com/gateway/dto/LoginResponse.java
@@ -6,40 +6,74 @@ public class LoginResponse {
 
 	private long expiresIn;
 
-	private String message;
+	private String email;
 
-	public LoginResponse(String token, long expiresIn) {
-		this.token = token;
-		this.expiresIn = expiresIn;
-	}
+	private String name;
 
-	public LoginResponse() {
-		// Default constructor
-	}
+	private String mobile;
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
+	private String role;
 
 	public String getToken() {
 		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
 	}
 
 	public long getExpiresIn() {
 		return expiresIn;
 	}
 
-	public LoginResponse setExpiresIn(long expiresIn) {
+	public void setExpiresIn(long expiresIn) {
 		this.expiresIn = expiresIn;
-		return this;
 	}
 
-	public LoginResponse setToken(String token) {
-		this.token = token;
-		return this;
+	public String getEmail() {
+		return email;
 	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public LoginResponse(String token, long expiresIn, String email, String name, String mobile, String role) {
+		super();
+		this.token = token;
+		this.expiresIn = expiresIn;
+		this.email = email;
+		this.name = name;
+		this.mobile = mobile;
+		this.role = role;
+	}
+
+	public LoginResponse() {
+		super();
+	}
+
 }
